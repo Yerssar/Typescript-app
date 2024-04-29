3913e82cd5382f06d418b09c5881b73fe7a18f87
Erbol <yers2106gmail.com>
2024-04-29 17:12:32 +0600

    add lesson10_practice

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import GlobalStyles from 'styles/GlobalStyles';
 // import Lesson06 from 'lessons/Lesson06/Lesson06';
 // import Lesson07 from 'lessons/Lesson07/Lesson07';
 // import Lesson08 from 'lessons/Lesson08/Lesson08';
-import Lesson09 from 'lessons/Lesson09/Lesson09';
+// import Lesson09 from 'lessons/Lesson09/Lesson09';
+import Lesson10 from 'lessons/Lesson10_practice/Lesson10';
+
 
 // Homeworks
 // import Homework07 from 'homeworks/Homework07/Homework07'
@@ -24,8 +26,10 @@ function App() {
       {/* <Homework07 /> */}
       {/* <Lesson08 /> */}
       {/* <Homework08 /> */}
-      <Lesson09 />
+      {/* <Lesson09 /> */}
       {/* <Homework09 /> */}
+      <Lesson10 />
+
     </>
   );
 }
